455557678a05e0c24961fe248b43c0c427bc1647
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2023-01-17 18:43:17 -0500

    built out the structure of the CurrentWeather component

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,5 +1,13 @@
 import "../current-weather.css";
 
 export default function CurrentWeather() {
-  return "sup";
+  return (
+    <div className="weather">
+      <div className="top">
+        <p className="city-name">Los Angeles</p>
+        <p className="weather-description">Foggy</p>
+      </div>
+      <img src="icons/01d.png" alt="weather" className="weather-icon" />
+    </div>
+  );
 }
