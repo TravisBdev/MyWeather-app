9c17a0e282a12df9068f52a05520a3f70bf7268a
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2023-01-18 15:49:37 -0500

    looped over the seven day forecast to render the icon associated with the weather for each day

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -14,7 +14,15 @@ export default function Forecast({ data }) {
         {data.list.slice(0, 7).map((item, index) => (
           <AccordionItem key={index}>
             <AccordionItemHeading>
-              <AccordionItemButton>YO</AccordionItemButton>
+              <AccordionItemButton>
+                <div className="dailyItem">
+                  <img
+                    src={`icons/${item.weather[0].icon}.png`}
+                    alt="weather forecast"
+                    className="small-icon"
+                  />
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
