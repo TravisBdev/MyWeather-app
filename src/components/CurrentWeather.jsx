0779d0a4a18062b3622b423ebbd4d9a734c64c02
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2023-01-18 13:03:55 -0500

    finished building out the skeleton for the current weather component (Temp-Details-RealFeel)

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -4,10 +4,24 @@ export default function CurrentWeather() {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city-name">Los Angeles</p>
-        <p className="weather-description">Foggy</p>
+        <div>
+          <p className="city-name">Los Angeles</p>
+          <p className="weather-description">Foggy</p>
+        </div>
+        <img src="icons/01d.png" alt="weather" className="weather-icon" />
       </div>
-      <img src="icons/01d.png" alt="weather" className="weather-icon" />
+      <div className="bottom">
+        <p className="temp">65°F</p>
+        <div className="details">
+          <div className="param-row">
+            <span className="param-label">Details</span>
+          </div>
+          <div className="param-row">
+            <span className="param-label">Real Feel</span>
+            <span className="param-value">75°F</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
