ef08e0168116f0c9eacfdd998b000c19bc45dc90
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2023-01-18 14:49:32 -0500

    received data then rendered that data in the corresponding elements of the currentWeather card

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,24 +1,44 @@
 import "../current-weather.css";
 
-export default function CurrentWeather() {
+export default function CurrentWeather({ data }) {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city-name">Los Angeles</p>
-          <p className="weather-description">Foggy</p>
+          <p className="city-name">{data.city}</p>
+          <p className="weather-description">({data.weather[0].description})</p>
         </div>
-        <img src="icons/01d.png" alt="weather" className="weather-icon" />
+        <img
+          src={`icons/${data.weather[0].icon}.png`}
+          alt="weather"
+          className="weather-icon"
+        />
       </div>
       <div className="bottom">
-        <p className="temp">65°F</p>
+        <p className="temp">{Math.round(data.main.temp)}°F</p>
         <div className="details">
-          <div className="param-row">
+          {/* <div className="param-row">
             <span className="param-label">Details</span>
-          </div>
+          </div> */}
           <div className="param-row">
             <span className="param-label">Real Feel</span>
-            <span className="param-value">75°F</span>
+            <span className="param-value">
+              {Math.round(data.main.feels_like)} °F
+            </span>
+          </div>
+          <div className="param-row">
+            <span className="param-label">Wind Speed</span>
+            <span className="param-value">
+              {Math.round(data.wind.speed)} mph
+            </span>
+          </div>
+          <div className="param-row">
+            <span className="param-label">Humidity</span>
+            <span className="param-value">{data.main.humidity} %</span>
+          </div>
+          <div className="param-row">
+            <span className="param-label">Barometer</span>
+            <span className="param-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
